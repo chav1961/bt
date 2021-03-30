e6c72fafa0e6ab68f69f77b42d9c7147bd5f9103
chav1961 <chav1961@mail.ru>
2021-03-30 16:52:53 +0500

    DAily commit

--- bt.model/src/main/java/chav1961/bt/model/interfaces/UIModelMapper.java
+++ bt.model/src/main/java/chav1961/bt/model/interfaces/UIModelMapper.java
@@ -1,7 +1,11 @@
 package chav1961.bt.model.interfaces;
 
+import java.io.IOException;
+
+import chav1961.purelib.basic.exceptions.ContentException;
 import chav1961.purelib.ui.swing.interfaces.JComponentMonitor;
 
-public interface UIModelMapper extends ModelMapper, JComponentMonitor {
-
+public interface UIModelMapper<Data,S,T> extends ModelMapper, JComponentMonitor {
+	void download(S source, Data target) throws ContentException, IOException;
+	void upload(Data source, T target) throws ContentException, IOException;
 }
