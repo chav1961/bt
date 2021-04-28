40a182086808bf4c9df87f75a3edb769902fa157
chav1961 <chav1961@winmain>
2021-04-28 20:31:04 +0500

    Daily commit

---
+++ bt.mnemoed/src/main/java/chav1961/bt/mnemoed/entities/EntityAlignment.java
@@ -0,0 +1,13 @@
+package chav1961.bt.mnemoed.entities;
+
+public enum EntityAlignment {
+	NORTH, 
+	NORTH_EAST, 
+	EAST, 
+	SOUTH_EAST, 
+	SOUTH, 
+	SOUTH_WEST, 
+	WEST, 
+	NORTH_WEST, 
+	CENTER;
+}
