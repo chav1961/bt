56997f52662aa171ccf354ca739dbc7dda6f821a
chav1961 <chav1961@mail.ru>
2021-06-15 10:01:10 +0500

    DAily commit

--- bt.mnemort/src/main/java/chav1961/bt/mnemort/interfaces/EntitiesView.java
+++ bt.mnemort/src/main/java/chav1961/bt/mnemort/interfaces/EntitiesView.java
@@ -2,6 +2,6 @@ package chav1961.bt.mnemort.interfaces;
 
 import chav1961.bt.mnemort.entities.interfaces.EntityInterface;
 
-public interface EntitiesView<Canvas> {
-	void draw(Canvas canvas, int width, int height, int windowWidth, int windowHeight, EntityInterface... entity);
+public interface EntitiesView<Canvas,T> {
+	void draw(Canvas canvas, int width, int height, int windowWidth, int windowHeight, @SuppressWarnings("unchecked") EntityInterface<T>... entity);
 }
---
+++ bt.mnemort/src/main/java/chav1961/bt/mnemort/interfaces/ItemView.java
@@ -0,0 +1,5 @@
+package chav1961.bt.mnemort.interfaces;
+
+public interface ItemView <Canvas> {
+	void draw(Canvas canvas, int width, int height);
+}
