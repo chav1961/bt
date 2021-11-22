290f95b1286910da4711c4468871fd2c37e8e680
chav1961 <chav1961@winmain>
2021-11-22 08:28:33 +0500

    Daily commit

---
+++ bt.comm/src/main/java/chav1961/bt/comm/FazecastWrapper.java
@@ -0,0 +1,18 @@
+package chav1961.bt.comm;
+
+import javax.comm.CommDriver;
+import javax.comm.CommPort;
+
+public class FazecastWrapper implements CommDriver {
+	@Override
+	public CommPort getCommPort(final String portName, final int portType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+}
