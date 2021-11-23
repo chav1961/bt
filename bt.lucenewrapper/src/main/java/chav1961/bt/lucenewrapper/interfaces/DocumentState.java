2eb740f2037ab823cc047c7a311b34c1a7c3e323
chav1961 <chav1961@winmain>
2021-11-23 15:37:29 +0500

    Daily commit

--- bt.lucenewrapper/src/main/java/chav1961/bt/lucenewrapper/interfaces/DocumentState.java
+++
@@ -1,8 +0,0 @@
-package chav1961.bt.lucenewrapper.interfaces;
-
-public enum DocumentState {
-	UNKNOWN,
-	NEW,
-	AVAILABLE,
-	IN_DEVELOPMENT
-}
--- bt.lucenewrapper/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module chav1961.bt.lucenewrapper {
-	requires chav1961.purelib;
-	requires lucene.core;
-	requires lucene.queryparser;
-
-	exports chav1961.bt.lucenewrapper;
-	exports chav1961.bt.lucenewrapper.interfaces;
-}
