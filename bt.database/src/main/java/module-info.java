2f86a8548bc49dccfaf3e555cb20839c6f056ddf
chav1961 <chav1961@mail.ru>
2022-03-31 16:06:02 +0500

    Dailycommit

---
+++ bt.database/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module chav1961.bt.database {
+	exports chav1961.bt.database;
+	exports chav1961.bt.database.interfaces;
+	
+	requires transitive java.sql;
+	requires transitive chav1961.purelib;
+}
