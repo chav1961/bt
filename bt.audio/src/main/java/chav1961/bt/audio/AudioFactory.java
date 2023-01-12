d1f6938da292e50a0407562f2f325a8e7ebe8204
chav1961 <chav1961@mail.ru>
2023-01-12 16:47:27 +0500

    Daily commit

--- bt.audio/src/main/java/chav1961/bt/audio/AudioFactory.java
+++ bt.audio/src/main/java/chav1961/bt/audio/AudioFactory.java
@@ -1,19 +1,30 @@
 package chav1961.bt.audio;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import javax.sound.midi.MidiDevice.Info;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.spi.MidiFileReader;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 
+import chav1961.purelib.basic.Utils;
+import chav1961.purelib.basic.growablearrays.GrowableByteArray;
 import chav1961.purelib.concurrent.interfaces.ExecutionControl;
 
 public class AudioFactory implements AutoCloseable, ExecutionControl {
 	public enum Priority {
 		
 	}
+	
+	private final Map<AudioFormat, ?>	audioChannels = new HashMap<>(); 
 	
 	public AudioFactory() {
 		this(true, true);
@@ -74,19 +85,67 @@ public class AudioFactory implements AutoCloseable, ExecutionControl {
 		// TODO Auto-generated method stub
 	}
 	
-	public void add(final String name, final AudioInputStream ais) {
+	public void add(final String name, final AudioInputStream ais) throws IOException {
+		if (Utils.checkEmptyOrNullString(name)) {
+			throw new IllegalArgumentException("Name can't be null or empty"); 
+		}
+		else if (ais == null) {
+			throw new NullPointerException("Audio input stream can't be null"); 
+		}
+		else {
+			final AudioFormat	af = ais.getFormat();
+			
+			try(final ByteArrayOutputStream	baos = new ByteArrayOutputStream();
+				final GZIPOutputStream		gzos = new GZIPOutputStream(baos)) {
+				
+				Utils.copyStream(ais, gzos);
+				placeSampleContent(name, af, baos.toByteArray());
+			}
+		}
+	}
+
+	private void placeSampleContent(final String name, final AudioFormat af, final byte[] content) {
+		// TODO Auto-generated method stub
 		
 	}
+
 
 	public void add(final String name, final MidiFileReader rdr) {
-		
+		if (Utils.checkEmptyOrNullString(name)) {
+			throw new IllegalArgumentException("Name can't be null or empty"); 
+		}
+		else if (rdr == null) {
+			throw new NullPointerException("MIDI reader can't be null"); 
+		}
+		else {
+			
+		}
 	}
 
-	public void add(final ZipInputStream ais) {
-		
+	public void add(final ZipInputStream zis) throws IOException {
+		add("", "*.*", zis);
 	}
 	
-	public void add(final String prefix, final ZipInputStream zis) {
+	public void add(final String prefix, final String mask, final ZipInputStream zis) throws IOException {
+		if (prefix == null) {
+			throw new NullPointerException("Prefix string can't be null"); 
+		}
+		else if (Utils.checkEmptyOrNullString(mask)) {
+			throw new IllegalArgumentException("Mask string can't be null or empty"); 
+		}
+		else if (zis == null) {
+			throw new NullPointerException("ZIP input stream can't be null"); 
+		}
+		else {
+			final Pattern	pattern = Pattern.compile(Utils.fileMask2Regex(mask));
+			ZipEntry		ze;
+			
+			while ((ze = zis.getNextEntry()) != null) {
+				if (pattern.matcher(ze.getName()).matches()) {
+					
+				}
+			}
+		}
 		
 	}
 
--- bt.audio/src/main/java/module-info.java
+++ bt.audio/src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module chav1961.bt.audio {
 	requires transitive chav1961.purelib;
+	requires java.desktop;
 }
