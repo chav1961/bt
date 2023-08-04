9ae47decd7f04850af09f9077457935eade1c09d
chav1961 <chav1961@mail.ru>
2023-08-04 15:22:24 +0500

    Daily commit

---
+++ bt.voice/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module chav1961.bt.voice {
+	requires transitive chav1961.purelib;
+	requires java.base;
+	requires java.desktop;
+	requires java.datatransfer;
+	requires transitive vosk;
+	requires com.sun.jna;
+}
