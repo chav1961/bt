691e4b7bb8a061fd07f32928001ab35a4c7ee2aa
chav1961 <chav1961@mail.ru>
2024-07-17 16:43:44 +0500

    Daily commit

--- bt.winsl/src/test/java/chav1961/bt/winsl/utils/JavaServiceLibraryTest.java
+++ bt.winsl/src/test/java/chav1961/bt/winsl/utils/JavaServiceLibraryTest.java
@@ -11,7 +11,7 @@ public class JavaServiceLibraryTest {
 	volatile int 	val;
 
 	@Test
-	@Ignore
+//	@Ignore
 	public void serviceLifeCycleTest() throws EnvironmentException, InterruptedException, ContentException {
 		final JavaServiceDescriptor	desc = new JavaServiceDescriptor();
 		
@@ -27,7 +27,8 @@ public class JavaServiceLibraryTest {
 		desc.lpServiceStartName = null;
 		desc.lpPassword = null;
 		
-		try{ Assert.assertEquals(0,JavaServiceLibrary.installService(desc));
+		try{ 
+			Assert.assertEquals(0,JavaServiceLibrary.installService(desc));
 
 		} finally {
 			Assert.assertEquals(0,JavaServiceLibrary.removeService("test"));
