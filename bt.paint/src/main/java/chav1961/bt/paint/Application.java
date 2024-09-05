bc2b4dafd0718f7f8af0471969513a2f441d241b
chav1961 <chav1961@mail.ru>
2024-09-05 12:37:48 +0500

    Daily commit

--- bt.paint/src/main/java/chav1961/bt/paint/Application.java
+++ bt.paint/src/main/java/chav1961/bt/paint/Application.java
@@ -88,7 +88,6 @@ import chav1961.purelib.i18n.interfaces.SupportedLanguages;
 import chav1961.purelib.model.ContentModelFactory;
 import chav1961.purelib.model.interfaces.ContentMetadataInterface;
 import chav1961.purelib.model.interfaces.ContentMetadataInterface.ContentNodeMetadata;
-import chav1961.purelib.nanoservice.NanoServiceFactory;
 import chav1961.purelib.model.interfaces.NodeMetadataOwner;
 import chav1961.purelib.ui.interfaces.LRUPersistence;
 import chav1961.purelib.ui.swing.SwingUtils;
@@ -1152,14 +1151,11 @@ public class Application extends JFrame implements NodeMetadataOwner, LocaleChan
 	private static void startGUI(final ApplicationMode mode, final ContentMetadataInterface xda, final Localizer localizer, final Predefines predef) throws InterruptedException, IOException, PaintScriptException {
 		final CountDownLatch	latch = new CountDownLatch(1);
 		
-		try(final Application			app = new Application(mode, xda, localizer, predef, latch);
-			final NanoServiceFactory	service = new NanoServiceFactory(PureLibSettings.SYSTEM_ERR_LOGGER, app.getSettings().getProps())) {
+		try(final Application			app = new Application(mode, xda, localizer, predef, latch)) {
 
-			service.start();
-			app.setHelpPort(service.getServerAddress().getPort());
+			app.setHelpPort(9999);
 			app.setVisible(true);
 			latch.await();
-			service.stop();
 		} catch (ContentException exc) {
 			throw new PaintScriptException(exc);
 		}
@@ -1171,8 +1167,7 @@ public class Application extends JFrame implements NodeMetadataOwner, LocaleChan
 			new StringArg(ARG_INPUT_MASK, false, false, "Mask for input files. If typed, all content will be get from file system instead of stdin"),
 			new BooleanArg(ARG_RECURSION_FLAG, false, "Process input mask recursively, if types", false),
 			new BooleanArg(ARG_GUI_FLAG, false, "Start GUI after processing. If any arguments are missing, starts GUI automatically", false),
-			new FileArg(ARG_PROPFILE_LOCATION, false, "Property file location", "./.bt.paint.properties"),
-			new IntegerArg(NanoServiceFactory.NANOSERVICE_PORT, false, "Process input mask recursively, if types", 0),
+			new FileArg(ARG_PROPFILE_LOCATION, false, "Property file location", "./.bt.paint.properties")
 		};
 		
 		private ApplicationArgParser() {
@@ -1187,11 +1182,6 @@ public class Application extends JFrame implements NodeMetadataOwner, LocaleChan
 		private Settings(final File config) throws IOException {
 			this.file = config;
 			this.props = new SubstitutableProperties();
-			
-			props.setProperty(NanoServiceFactory.NANOSERVICE_PORT, "0");
-			props.setProperty(NanoServiceFactory.NANOSERVICE_ROOT, FileSystemInterface.FILESYSTEM_URI_SCHEME+":xmlReadOnly:root://"+Application.class.getCanonicalName()+"/chav1961/bt/paint/helptree.xml");
-			props.setProperty(NanoServiceFactory.NANOSERVICE_CREOLE_PROLOGUE_URI, Application.class.getResource("prolog.cre").toString()); 
-			props.setProperty(NanoServiceFactory.NANOSERVICE_CREOLE_EPILOGUE_URI, Application.class.getResource("epilog.cre").toString());
 			
 			if (config.exists() && config.isFile() && config.canRead()) {
 				try(final InputStream	is = new FileInputStream(config)) {
