86ae81ae22e20c964faed69cbfb28a49703132f8
chav1961 <chav1961@mail.ru>
2024-11-20 14:38:33 +0500

    Daily commit

---
+++ bt.nlp/src/main/java/chav1961/bt/nlp/parser/LemmatizeReader.java
@@ -0,0 +1,22 @@
+package chav1961.bt.nlp.parser;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class LemmatizeReader extends Reader {
+	public LemmatizeReader(final Reader nested) {
+		
+	}
+
+	@Override
+	public int read(char[] cbuf, int off, int len) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ bt.nlp/src/main/java/chav1961/bt/nlp/parser/TokenReader.java
@@ -0,0 +1,17 @@
+package chav1961.bt.nlp.parser;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.Reader;
+
+public class TokenReader implements Closeable {
+	public TokenReader(final Reader content) {
+		
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+}
