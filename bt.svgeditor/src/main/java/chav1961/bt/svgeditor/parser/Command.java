f13a4a831d4c656535568aca3e2d1314c43c5bfa
chav1961 <chav1961@mail.ru>
2025-09-30 15:02:31 +0500

    Daily commit

--- bt.svgeditor/src/main/java/chav1961/bt/svgeditor/parser/Command.java
+++ bt.svgeditor/src/main/java/chav1961/bt/svgeditor/parser/Command.java
@@ -1,6 +1,18 @@
 package chav1961.bt.svgeditor.parser;
 
+import java.util.function.BiConsumer;
+
+import chav1961.purelib.basic.Utils;
+
 public class Command {
+	public static enum CommanType {
+		NEW_ENTITY,
+		TRANSFORM_ENTITY,
+		REMOVE_ENTITY,
+		PROPERTIES_CHANGE,
+		SELECTION
+	}
+	
 	// <Descriptor>	::= <Name>[<Parameter>...]
 	// <Name>		::= <Letter>[<LetterOrDigit>...]
 	// <Parameter>	::= <Name>':'<Type>['='[<DefaultValue>]]
@@ -16,9 +28,57 @@ public class Command {
 	// Example:
 	//	line from:point to:point color:color=green
 	
-	public Command(final String descriptor, final String helpPrefix, final String helpReference) {
-		
+	private final CommanType	type;
+	private final String		descriptor;
+	private final String		helpPrefix;
+	private final String		helpReference;
+	final BiConsumer<Command,Object[]>	consumer;
+	final Object[]				param;
+	
+	public Command(final CommanType type, final String descriptor, final String helpPrefix, final String helpReference, final BiConsumer<Command, Object[]> processor, final Object... parameters) {
+		if (type == null) {
+			throw new NullPointerException("Commnad type can't be null");
+		}
+		else if (Utils.checkEmptyOrNullString(descriptor)) {
+			throw new IllegalArgumentException("Command descriptor can be neither null nor empty");
+		}
+		else if (Utils.checkEmptyOrNullString(helpPrefix)) {
+			throw new IllegalArgumentException("Help prefix can be neither null nor empty");
+		}
+		else if (Utils.checkEmptyOrNullString(helpReference)) {
+			throw new IllegalArgumentException("Help reference can be neither null nor empty");
+		}
+		else if (processor == null) {
+			throw new NullPointerException("Processor can't be null");
+		}
+		else if (parameters == null || parameters.length == 0 || Utils.checkArrayContent4Nulls(parameters) >= 0) {
+			throw new IllegalArgumentException("Command parameters is null, empty or contains nulls inside");
+		}
+		else {
+			this.type = type;
+			this.descriptor = descriptor;
+			this.helpPrefix = helpPrefix;
+			this.helpReference = helpReference;
+			this.consumer = processor;
+			this.param = parameters;
+		}
 	}
 	
+	public CommanType getType() {
+		return type;
+	}
+
+	public String getDescriptor() {
+		return descriptor;
+	}
+
+	public String getHelpPrefix() {
+		return helpPrefix;
+	}
+
+	public String getHelpReference() {
+		return helpReference;
+	}
+
 	
 }
--- bt.svgeditor/src/main/java/chav1961/bt/svgeditor/parser/CommandLineParser.java
+++ bt.svgeditor/src/main/java/chav1961/bt/svgeditor/parser/CommandLineParser.java
@@ -1,5 +1,70 @@
 package chav1961.bt.svgeditor.parser;
 
-public class CommandLineParser {
+import chav1961.bt.svgeditor.parser.Command.CommanType;
+import chav1961.purelib.basic.CharUtils;
+import chav1961.purelib.basic.CharUtils.ArgumentType;
+import chav1961.purelib.basic.CharUtils.Optional;
+import chav1961.purelib.basic.Utils;
+import chav1961.purelib.basic.exceptions.CommandLineParametersException;
+import chav1961.purelib.basic.exceptions.SyntaxException;
 
+/*
+ * <Command> ::= {<SelectCommand>|<LineCommand>|<MoveCommand|<RotateCommand>|<ScaleCommand}
+ * 
+ * <SelectCommand> ::= 'select' {'+'|'-'} {'n'|'none'|'a'|'all'|'w'<Window>|'window'<Window>|'c'<Window>|'cross'<Window>}
+ * 
+ * <LineCommand> ::= 'line' <FromPoint> ['to'] <ToPoint>
+ * <FromPoint> ::= {<AbsolutePoint>|'last'|'l']}
+ * <ToPoint> ::= {<AbsolutePoint>|<RelativePoint>}
+ *
+ * <Window> ::= <AbsolutePoint> {<AbsolutePoint>|<RelativePoint>} 
+ * <AbsolutePoint> ::= {<int>','<int>}
+ * <RelativePoint> ::= {'@'<int>',''@'<int>}
+ */
+public class CommandLineParser {
+	static final Command[]		COMMANDS = {
+										new Command(CommanType.NEW_ENTITY, "line %1:point %2:point", "1", "1",
+												(c,v)->{}, 
+												ArgumentType.signedInt, ',', ArgumentType.signedInt, 
+												new Optional("to"),
+												ArgumentType.signedInt, ',', ArgumentType.signedInt 
+												)
+									};
+
+	private static final char	EOF = '\uFFFF';
+	
+	private final Command[]	commands;
+	
+	public CommandLineParser() {
+		this(COMMANDS);
+	}
+
+	public CommandLineParser(final Command... commands) {
+		// TODO Auto-generated constructor stub
+		if (commands == null || commands.length == 0 || Utils.checkArrayContent4Nulls(commands) >= 0) {
+			throw new IllegalArgumentException("Commands is null, empty or contains nulls inside");
+		}
+		else {
+			this.commands = commands;
+		}
+	}
+	
+	public void parse(final CharSequence descriptor) throws CommandLineParametersException {
+		// TODO Auto-generated method stub
+		final char[]	temp = CharUtils.terminateAndConvert2CharArray(descriptor, EOF);
+		
+		for(Command item : commands) {
+			try{
+				if (CharUtils.tryExtract(temp, 0, item.param) == temp.length) {
+					final Object[]	values = new Object[100];
+					
+					CharUtils.extract(temp, 0, values, item.param);
+					item.consumer.accept(item, values);
+					return;
+				}
+			} catch (SyntaxException e) {
+			}
+		}
+		throw new CommandLineParametersException("Unknown command");
+	}
 }
